297cba874e866136dd2cf9e70dfd76a7807fb8a6
Swastik Banerjee <swastikb06@gmail.com>
2023-07-07 16:13:22 +0530

    .

---
+++ 29_shuffle_Card_Deck.py
@@ -0,0 +1,11 @@
+# Program to randomly shuffle a deck of cards.
+
+import random, itertools
+
+deck = list(itertools.product(range(1,14),["Spade","Club","Hearts","Diamond"]))
+
+random.shuffle(deck)
+print(deck)
+
+for i in range(5):
+    print(deck[i][0],"of",deck[i][1])
